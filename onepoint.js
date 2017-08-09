2a4ac96ecb03e983c944220f35d3c4089d860924
tommytnez <tommytnez@yahoo.com>
2017-08-09 16:25:23 -0700

    checkPermute

--- onepoint.js
+++ onepoint.js
@@ -12,3 +12,25 @@ function isUnique (string) {
 }
 
 console.log(isUnique('abcde'));
+
+function checkPermutation (string1, string2) {
+  var str1 = {};
+  var str2 = {};
+  if (string1.length !== string2.length) {
+    return false;
+  }
+  for (var i = 0; i < string1.length; i++) {
+    str1 += string1[i];
+    console.log(str1);
+  }
+  for (var j = 0; j < string2.length; j++) {
+    str2 += string2[j];
+  }
+  console.log(str1, str2);
+  if (str1 !== str2) {
+    return false;
+  }
+  return true;
+}
+
+console.log(checkPermutation('able', 'able'));
