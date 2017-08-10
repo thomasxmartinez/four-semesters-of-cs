2223dac7949f57347de270e9f74c2660f41666ee
tommytnez <tommytnez@yahoo.com>
2017-08-09 18:45:22 -0700

    was wrong

--- onepoint.js
+++ onepoint.js
@@ -12,38 +12,3 @@ function isUnique (string) {
 }
 
 console.log(isUnique('abcde'));
-
-function checkPermutation (string1, string2) {
-  var str1 = {};
-  var str2 = {};
-  if (string1.length !== string2.length) {
-    return false;
-  }
-  for (var i = 0; i < string1.length; i++) {
-    str1 += string1[i];
-  }
-  for (var j = 0; j < string2.length; j++) {
-    str2 += string2[j];
-  }
-  if (str1 !== str2) {
-    return false;
-  }
-  return true;
-}
-
-console.log(checkPermutation('able', 'able'));
-
-function urlIfy (str) {
-  var newStr = '';
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] === ' ') {
-      newStr += '%20';
-      i++;
-    }
-    newStr += str[i];
-  }
-
-  return newStr;
-}
-
-console.log(urlIfy('I like to race horses'));
